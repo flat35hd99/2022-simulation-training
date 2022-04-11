b5225b44c688fb95873457df4723e79c4369722c
Wataru Sugiura <38929316+flat35hd99@users.noreply.github.com>
2022-04-11 04:05:56 +0000

    Add formatter

--- Lecture1/tasks/pi.cpp
+++ Lecture1/tasks/pi.cpp
@@ -1,26 +1,27 @@
-#include <stdio.h> // for printf, etc                                                                                                                                       
-#include <stdlib.h> // for rand(), etc                                                                                                                                      
-#include <math.h> // for sin(),cos(), etc                                                                                                                                   
-#include <iostream>// for cout, etc                                                                                                                                         
-#include <fstream> // for ifstream/ofstream                                                                                                                                 
-#include <time.h>// for time(NULL), etc    
-#include <iomanip>                                                                                                                                 
+#include <math.h>    // for sin(),cos(), etc
+#include <stdio.h>   // for printf, etc
+#include <stdlib.h>  // for rand(), etc
+#include <time.h>    // for time(NULL), etc
+
+#include <fstream>  // for ifstream/ofstream
+#include <iomanip>
+#include <iostream>  // for cout, etc
 using namespace std;
-int main(void){
+int main(void) {
   int i, count, max = 1e+9, observe_span = 1e+4;
-  double x,y,z,pi;
+  double x, y, z, pi;
 
   srand(1234);
 
-  for(i=0;i<max;i++){
-    x = (double)rand()/RAND_MAX;
-    y = (double)rand()/RAND_MAX;
-    z = x*x + y*y;
-    if(z<1){
+  for (i = 0; i < max; i++) {
+    x = (double)rand() / RAND_MAX;
+    y = (double)rand() / RAND_MAX;
+    z = x * x + y * y;
+    if (z < 1) {
       count++;
     }
-    if((i + 1) % observe_span == 0){
-      pi = (double) count / (i + 1) * 4;
+    if ((i + 1) % observe_span == 0) {
+      pi = (double)count / (i + 1) * 4;
       printf("%d,%.20f\n", i + 1, pi);
     }
   }
